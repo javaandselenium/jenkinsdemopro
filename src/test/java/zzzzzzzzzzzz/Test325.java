2bcaa0009adb3e3897423027828775c8e43e9e43
QSP <QSP@DESKTOP-EVK9GMC>
2021-03-05 12:47:11 +0530

    push1

---
+++ src/test/java/zzzzzzzzzzzz/Test325.java
@@ -0,0 +1,13 @@
+package zzzzzzzzzzzz;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+
+public class Test325 {
+	@Test
+	public void demo() {
+		Reporter.log("hello api",true);
+	}
+
+}
