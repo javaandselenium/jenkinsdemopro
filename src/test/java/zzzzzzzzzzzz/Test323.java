1fb021c8544e3e89ec4053d4997c07220071d934
QSP <QSP@DESKTOP-EVK9GMC>
2021-03-05 12:32:39 +0530

    Hello

---
+++ src/test/java/zzzzzzzzzzzz/Test323.java
@@ -0,0 +1,13 @@
+package zzzzzzzzzzzz;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+
+public class Test323 {
+	@Test
+	public void demo() {
+		Reporter.log("hello selenium",true);
+	}
+
+}
